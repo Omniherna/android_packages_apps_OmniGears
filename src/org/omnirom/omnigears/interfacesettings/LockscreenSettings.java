54dd118586e3e61e46b1b63dac4826e64526995c
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2015-12-06 13:19:50 +0100

    (2/2) OmniGears: Enable voice lockscreen shortcut
    Change-Id: I8c8ed9e842e1c62a21ac99d16538930653ffac56

--- src/org/omnirom/omnigears/interfacesettings/LockscreenSettings.java
+++ src/org/omnirom/omnigears/interfacesettings/LockscreenSettings.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-package org.omnirom.omnigears;
+package org.omnirom.omnigears.interfacesettings;
 
 import android.app.Activity;
 import android.app.WallpaperManager;
